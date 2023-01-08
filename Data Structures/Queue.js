a232897573069d28d4d3b10ec237d96babe1fd4e
Sajib <contact2sajib@gmail.com>
2023-01-08 18:46:49 +0600

    Queue uisng LinkedList

---
+++ Data Structures/Queue.js
@@ -0,0 +1,58 @@
+// Implementing Queue using Linked List
+
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Queue {
+    constructor(){
+      this.first = null;
+      this.last = null;
+      this.length = 0;
+    }
+    peek() {
+      return this.first;
+    }
+    enqueue(value){
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.first = newNode;
+        this.last = newNode;
+      } else {
+        this.last.next = newNode;
+        this.last = newNode;
+      }
+      this.length++;
+      return this;
+    }
+    dequeue(){
+      if (!this.first) {
+        return null;
+      }
+      if (this.first === this.last) {
+        this.last = null;
+      }
+      const holdingPointer = this.first;
+      this.first = this.first.next;
+      this.length--;
+      return this;
+    }
+    isEmpty(){
+        return !this.length
+    }
+  }
+  
+  const myQueue = new Queue();
+  console.log(myQueue.peek())
+  console.log(myQueue.enqueue('Hi,'))
+  console.log(myQueue.enqueue('I am'))
+  console.log(myQueue.enqueue('Sajib.'))
+  console.log(myQueue.peek())
+  console.log(myQueue.dequeue())
+  console.log(myQueue.dequeue())
+  console.log(myQueue.dequeue())
+  console.log(myQueue.peek())
+  console.log(myQueue.isEmpty())
